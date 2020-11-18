076ec84156f1aa061ac399cbe3bd3d0abf6fbbd2
Robin Nguyen <robinnguyenk@gmail.com>
2020-11-18 15:47:50 -0800

    ctof

--- src/edu/ucsd/cs110/temperature/Celsius.java
+++ src/edu/ucsd/cs110/temperature/Celsius.java
@@ -14,7 +14,7 @@ public class Celsius extends Temperature
     @Override
     public Temperature toFahrenheit() {
         float value = ((this.getValue()*9)/5) + 32;
-        return new Fahrenheit(value);
+        return new Celsius(value);
     }
 
     public String toString()
